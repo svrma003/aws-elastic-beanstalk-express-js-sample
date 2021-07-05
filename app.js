3d1f910111ce66cbe55535120859b05d238576bb
Sachin Verma <svrma007@gmail.com>
2021-07-05 15:21:57 +0530

    modified app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Learn Continuous Delivery Pipeline in AWS - Full Pipeline Test'));
+app.get('/', (req, res) => res.send('Learn CI/CD Pipeline in AWS - Full Pipeline Test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
